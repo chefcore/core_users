6bc0beeb0db6c8978f7b2b3a9b4ca186866956e7
Levon Becker <levon+github@bonusbits.com>
2015-08-10 20:40:01 -0700

    Initial Release

---
+++ README.md
@@ -0,0 +1,11 @@
+# Chef Core Cookbook
+
+## Description
+Used to install/configure security related services such as firewall and antivirus.
+
+## Supported Platforms
+* Linux
+* Windows
+
+## Testing
+Use core_base to test this cookbook.
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+# rubocop:disable Style/SingleSpaceBeforeFirstArg
+
+name             'core_users'
+maintainer       'Bonus Bits'
+maintainer_email 'devops@bonusbits.com'
+license          'MIT'
+description      'Wrapper Cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
